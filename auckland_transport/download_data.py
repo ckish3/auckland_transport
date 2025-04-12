4679a233f56a5ee9760cc85edd3b02933295868f
ckish3 <ckish1@gmail.com>
2025-04-12 13:35:04 -0700

    add test for reponse conversion

---
+++ auckland_transport/download_data.py
@@ -0,0 +1,79 @@
+from collections import defaultdict
+
+import pandas as pd
+import requests
+import os
+
+
+def make_realtime_request() -> dict:
+
+    """
+    Make a request to the Auckland Transport API's real-time feed, and return
+    the response data as a dictionary.
+
+    Returns:
+        dict: The real-time data from the feed.
+    """
+    url = 'https://api.at.govt.nz/realtime/legacy/tripupdates'
+    key = os.environ['AUCKLAND_TRANSPORT_API_KEY']
+    headers = {
+        'Cache-Control': 'no-cache',
+        'Ocp-Apim-Subscription-Key': key
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code != 200:
+        raise Exception(f"Request failed with status code {response.status_code}")
+
+    data = response.json()
+    if data['status'] != 'OK':
+        raise Exception(f"Request failed with status {data['status']}")
+
+    return data['response']
+
+
+
+def convert_request_to_df(data: dict) -> pd.DataFrame:
+    """
+    Convert the real-time data from the Auckland Transport API into a pandas
+    DataFrame.
+
+    Args:
+        data (dict): The real-time data from the Auckland Transport API.
+
+    Returns:
+        pd.DataFrame: The real-time data in a pandas DataFrame.
+    """
+
+    entities = data['entity']
+    formatted_dict = defaultdict(list)
+
+    for e in entities:
+        try:
+            row = {
+                'trip_id': e['trip_update']['trip']['trip_id'],
+                'route_id': e['trip_update']['trip']['route_id'],
+                'direction_id': e['trip_update']['trip']['direction_id'],
+                'start_time': e['trip_update']['trip']['start_time'],
+                'start_date': e['trip_update']['trip']['start_date'],
+                'stop_id': e['trip_update']['stop_time_update']['stop_id'],
+                'stop_time': e['trip_update']['stop_time_update']['arrival']['time'],
+                'stop_delay': e['trip_update']['stop_time_update']['arrival']['delay'],
+                'stop_uncertainty': e['trip_update']['stop_time_update']['arrival']['uncertainty'],
+                'stop_sequence': e['trip_update']['stop_time_update']['stop_sequence'],
+                'vehicle_id': e['trip_update']['vehicle']['id'],
+                'vehicle_licence_plate': e['trip_update']['vehicle']['license_plate'],
+                'trip_delay': e['trip_update']['delay'],
+                'timestamp': e['trip_update']['timestamp'],
+            }
+
+            for k, v in row.items():
+                formatted_dict[k].append(v)
+
+        except KeyError as e:
+            print(f"Error: {e}")
+            continue
+
+    df = pd.DataFrame(formatted_dict)
+    return df
---
+++ tests/test_download_data.py
@@ -0,0 +1,131 @@
+
+import json
+import pandas as pd
+
+import auckland_transport.download_data as download_data
+
+def test_convert_request_to_df():
+    input = """
+    {"response": {
+        "header": {
+            "timestamp": 1744485168.859,
+            "gtfs_realtime_version": "1.0",
+            "incrementality": 0
+        },
+        "entity": [{
+            "id": "902-98012-34200-1-063671ab",
+            "trip_update": {
+                "trip": {
+                    "trip_id": "902-98012-34200-1-063671ab",
+                    "start_time": "09:30:00",
+                    "start_date": "20250413",
+                    "schedule_relationship": 3,
+                    "route_id": "MTIA-209",
+                    "direction_id": 1
+                },
+                "timestamp": 1744452776,
+                "delay": 0
+            },
+            "is_deleted": false
+        }, {
+            "id": "902-98011-37800-1-d2486685",
+            "trip_update": {
+                "trip": {
+                    "trip_id": "902-98011-37800-1-d2486685",
+                    "start_time": "10:30:00",
+                    "start_date": "20250413",
+                    "schedule_relationship": 3,
+                    "route_id": "MTIA-209",
+                    "direction_id": 0
+                },
+                "timestamp": 1744452798,
+                "delay": 0
+            },
+            "is_deleted": false
+        }, {
+            "id": "1397-38017-18600-2-1c8a525c",
+            "trip_update": {
+                "trip": {
+                    "trip_id": "1397-38017-18600-2-1c8a525c",
+                    "start_time": "05:10:00",
+                    "start_date": "20250413",
+                    "schedule_relationship": 0,
+                    "route_id": "AIR-221",
+                    "direction_id": 0
+                },
+                "stop_time_update": {
+                    "stop_sequence": 24,
+                    "arrival": {
+                        "delay": 1642,
+                        "time": 1744482442,
+                        "uncertainty": 0
+                    },
+                    "stop_id": "1808-0d9a1f7f",
+                    "schedule_relationship": 0
+                },
+                "vehicle": {
+                    "id": "26008",
+                    "label": "GB8904",
+                    "license_plate": "NFS279"
+                },
+                "timestamp": 1744482459,
+                "delay": 1642
+            },
+            "is_deleted": false
+        }, {
+            "id": "1021-11147-19560-2-4ce78e52",
+            "trip_update": {
+                "trip": {
+                    "trip_id": "1021-11147-19560-2-4ce78e52",
+                    "start_time": "05:26:00",
+                    "start_date": "20250413",
+                    "schedule_relationship": 0,
+                    "route_id": "RBW-402",
+                    "direction_id": 0
+                },
+                "stop_time_update": {
+                    "stop_sequence": 17,
+                    "arrival": {
+                        "delay": -499,
+                        "time": 1744483481,
+                        "uncertainty": 0
+                    },
+                    "stop_id": "7016-875f30cb",
+                    "schedule_relationship": 0
+                },
+                "vehicle": {
+                    "id": "23633",
+                    "label": "PC4633",
+                    "license_plate": "KPK176"
+                },
+                "timestamp": 1744484818,
+                "delay": -499
+            },
+            "is_deleted": false
+        }]
+    }
+}"""
+
+    expected = {
+            "trip_id": ["1397-38017-18600-2-1c8a525c", "1021-11147-19560-2-4ce78e52"],
+            "start_time": ["05:10:00", "05:26:00"],
+            "start_date": ["20250413", "20250413"],
+            "route_id": ["AIR-221", "RBW-402"],
+            "direction_id": [0, 0],
+            "stop_sequence": [24, 17],
+            "arrival_delay": [1642, -499],
+            "arrival_time": [1744482442, 1744483481],
+            "arrival_uncertainty": [0, 0],
+            "stop_id": ["1808-0d9a1f7f", "7016-875f30cb"],
+            "vehicle_id": ["26008", "23633"],
+            "vehicle_license_plate": ["NFS279", "KPK176"],
+            "timestamp": [1744482459, 1744484818],
+            "trip_delay": [1642, -499]
+        }
+    input = json.loads(input)['response']
+    expected = pd.DataFrame(expected)
+    df = download_data.convert_request_to_df(input)
+    print(df)
+#    assert df.equals(expected)
+    assert len(df.index) == 2
+

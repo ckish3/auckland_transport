1000064c9ee96729acbb46b97345c94c8ffaab4e
ckish3 <ckish1@gmail.com>
2025-04-25 18:25:36 -0700

    rename hour column

--- auckland_transport/dbt_auckland_transport/models/processed_data/delay.sql
+++ auckland_transport/dbt_auckland_transport/models/processed_data/delay.sql
@@ -2,7 +2,7 @@ SELECT
     *,
     stop_delay / 60.0 as minutes_delay,
     CASE WHEN stop_delay < -5*60 0 THEN 'early'
-        WHEN stop_delay > 10*60 THEN '10+ minute delay'
-        WHEN stop_delay > 5*60 THEN '5-10 minute delay'
+        WHEN stop_delay >= 10*60 THEN '10+ minute delay'
+        WHEN stop_delay > 5*60 AND stop_delay < 10*60 THEN '5-10 minute delay'
         ELSE 'on time' END as delay_category
 FROM {{ ref('time_columns') }}
--- auckland_transport/dbt_auckland_transport/models/processed_data/time_columns.sql
+++ auckland_transport/dbt_auckland_transport/models/processed_data/time_columns.sql
@@ -1,7 +1,7 @@
 
 SELECT
     *,
-    date_part('hour', to_timestamp(epoch_time)) as hour,
+    date_part('hour', to_timestamp(epoch_time)) as response_hour,
     EXTRACT(DOW FROM to_timestamp(epoch_time)) as day_of_week,
     CASE WHEN EXTRACT(DOW FROM to_timestamp(epoch_time)) = 0 OR EXTRACT(DOW FROM to_timestamp(epoch_time)) = 6 THEN 'weekend'
         ELSE 'weekday' END as day_type,

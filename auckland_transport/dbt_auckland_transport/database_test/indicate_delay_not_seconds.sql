5c43cf3f433dd38c6fdc8c25000892418632f78c
ckish3 <ckish1@gmail.com>
2025-05-16 12:39:52 -0700

    add test indicating delay is in minutes

---
+++ auckland_transport/dbt_auckland_transport/database_test/indicate_delay_not_seconds.sql
@@ -0,0 +1,14 @@
+--This test indicates whether the delay is in units larger than minutes. IT IS NOT POSSIBLE TO
+--BE DEFINITIVE, SO A FAILURE DOES NOT MEAN THAT THE UNITS ARE DEFINITELY NOT MINUTES AND
+--THIS TEST PASSING DOES NOT MEAN THAT THE UNITS ARE NOT IN SECONDS
+
+WITH avg_delay as (
+    SELECT
+        MAX(minutes_delay) as delay_time
+    FROM {{ ref('delay') }}
+)
+
+SELECT
+    *
+FROM avg_delay
+WHERE delay_time > 24*60*60

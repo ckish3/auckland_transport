78aab5bc333c2c6bbdf8fd3c30dcdb79b078376a
ckish3 <ckish1@gmail.com>
2025-05-16 11:42:16 -0700

    add trip uniqueness test

---
+++ auckland_transport/dbt_auckland_transport/database_test/trip_update_unique.sql
@@ -0,0 +1,22 @@
+
+with count_rows as (
+    SELECT
+        route_id,
+        direction_id,
+        start_time,
+        start_date,
+        stop_id,
+        COUNT(*) as num_rows
+    FROM {{ ref('trip_update') }}
+    GROUP BY
+        route_id,
+        direction_id,
+        start_time,
+        start_date,
+        stop_id
+
+)
+SELECT
+    *
+FROM count_rows
+WHERE num_rows > 1
